f2dd1a04a4edfb42553173f8ab03de09aa408ef5
GiandoSaba <giandosaba997@gmail.com>
2022-02-11 16:56:31 +0100

    header end

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,3 +1,37 @@
 <header>
-    <h1>Header</h1>
+    <div class="header-top">
+        <div class="container">
+            <div>
+                DC POWER&trade; VISA&reg;
+            </div>
+            <div>
+               ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i>
+            </div>
+        </div>
+    </div>
+    <div class="header-menu">
+        <div class="container">
+            <div class="logo">
+                <img src="{{asset('img/dc-logo.png')}}" alt="Logo">
+            </div>
+            <nav>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li class="active">COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP <i class="fa-solid fa-caret-down"></i></li>
+                </ul>
+            </nav>
+            <div class="search">
+                <input type="text" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </div>
 </header>
