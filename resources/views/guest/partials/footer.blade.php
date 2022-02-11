f7a68fd1a763b580871ecef1d3fb5edae676ddf0
GiandoSaba <giandosaba997@gmail.com>
2022-02-11 18:50:55 +0100

    end footer

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -1,3 +1,97 @@
 <header>
-    <h1>Footer</h1>
+    <div class="footer-card">
+        <div class="container">
+            <div class="footer-card-item">
+                <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="buy-comics-digital-comics">
+                <span>Digital Comics</span>
+            </div>
+            <div class="footer-card-item">
+                <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="buy-comics-merchandise">
+                <span>DC Merchandise</span>
+            </div>
+            <div class="footer-card-item">
+                <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="buy-comics-subscriptions">
+                <span>subscriptions</span>
+            </div>
+            <div class="footer-card-item">
+                <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="buy-comics-shop-locator">
+                <span>comic shop locator</span>
+            </div>
+            <div class="footer-card-item">
+                <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="buy-dc-power-visa">
+                <span>dc power visa</span>
+            </div>
+        </div>
+    </div>
+    <div class="footer-main-content">
+        <div class="container">
+            <div class="footer-link">
+                <div class="link-col">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="link-col">
+                    <h3>DC</h3>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contacts Us</li>
+                    </ul>
+                </div>
+                <div class="link-col">
+                    <h3>SITES</h3>
+                    <ul>
+                        <li>Dc</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                     </ul>
+                </div>
+                <div class="cookie">
+                    <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span>noted here</span>. All rights reserved.
+                    <br><span>Cookies Settings</span></p>
+                </div>
+            </div>
+            <div class="footer-img">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="sign-up">
+                <h2>SIGN-UP NOW!</h2>
+            </div>
+            <div class="social-link">
+                <h3>FOLLOW US</h3>
+                <img src="{{asset('img/footer-facebook.png')}}" alt="Facebook">
+                <img src="{{asset('img/footer-twitter.png')}}" alt="Twitter">
+                <img src="{{asset('img/footer-youtube.png')}}" alt="Youtube">
+                <img src="{{asset('img/footer-pinterest.png')}}" alt="Pinterest">
+                <img src="{{asset('img/footer-periscope.png')}}" alt="Periscope">
+            </div>
+        </div>
+    </div>
 </header>
