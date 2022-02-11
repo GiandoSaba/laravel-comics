4db1febacc8a3b58790d3f7f704f645f488c296c
GiandoSaba <giandosaba997@gmail.com>
2022-02-11 18:05:13 +0100

    end main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,26 @@
 
 @section('content')
   <main>
-    <h1>Siamo in home</h1>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>CURRENT SERIES</h1>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="container cards">
+            @foreach ($comics as $comic)
+            <div class="card">
+                <div class="img-container">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                </div>
+                <h1>{{ $comic['series'] }}</h1>
+            </div>
+            @endforeach
+        </div>
+        <div class="more-content">
+            <h2>Load More</h2>
+        </div>
+    </div>
   </main>
 @endsection
 
